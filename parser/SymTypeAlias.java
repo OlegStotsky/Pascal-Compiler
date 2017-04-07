35350e2e258552fd20728f96a9e34af0567c5fbb
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-07 15:54:35 +1000

    add class SymTypeAlias, add class SymTypePointer

---
+++ parser/SymTypeAlias.java
@@ -0,0 +1,16 @@
+package parser;
+
+/**
+ * Created by olegstotsky on 07.04.17.
+ */
+public class SymTypeAlias extends SymType {
+    public SymType refType;
+
+    public SymTypeAlias() {
+        this.refType = null;
+    }
+
+    public SymTypeAlias(SymType refType) {
+        this.refType = refType;
+    }
+}
---
+++ parser/SymTypePointer.java
@@ -0,0 +1,16 @@
+package parser;
+
+/**
+ * Created by olegstotsky on 07.04.17.
+ */
+public class SymTypePointer extends SymType {
+    public SymType refType;
+
+    public SymTypePointer() {
+        this.refType = null;
+    }
+
+    public SymTypePointer(SymType refType) {
+        this.refType = refType;
+    }
+}
