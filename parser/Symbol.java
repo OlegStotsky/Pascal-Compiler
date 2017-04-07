0939ba6451995f8d653cb6ef482ac3e3c4b1d245
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-07 15:34:34 +1000

    add class Symbol

---
+++ parser/Symbol.java
@@ -0,0 +1,16 @@
+package parser;
+
+/**
+ * Created by olegstotsky on 07.04.17.
+ */
+public class Symbol {
+    public String name;
+
+    public Symbol() {
+        this.name = null;
+    }
+
+    public Symbol(String name) {
+        this.name = name;
+    }
+}
