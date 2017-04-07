2b6efc2500b8311022c5803640475117b6387412
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-07 17:17:56 +1000

    add class SymFunc

---
+++ parser/SymFunc.java
@@ -0,0 +1,19 @@
+package parser;
+
+import java.util.ArrayList;
+
+/**
+ * Created by olegstotsky on 07.04.17.
+ */
+public class SymFunc extends SymProc {
+    public SymType returnType;
+
+    public SymFunc() {
+        this.returnType = null;
+    }
+
+    public SymFunc(SymType returnType, ArrayList<SymVar> args, ArrayList<SymVar> locals) {
+        super(args, locals);
+        this.returnType = returnType;
+    }
+}
