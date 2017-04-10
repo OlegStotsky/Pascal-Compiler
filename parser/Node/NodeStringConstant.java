81b2106862a13d6f78df5c98d1b7f26d9c2401cc
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-10 12:03:34 +1000

    add NodeStringCostant constructor, add method NodeStringConstant.print

--- parser/Node/NodeStringConstant.java
+++ parser/Node/NodeStringConstant.java
@@ -1,7 +1,18 @@
 package parser.Node;
 
 public class NodeStringConstant extends Node {
+	public String value;
+
 	public NodeStringConstant() {
-		
+		this.value = null;
+	}
+
+	public NodeStringConstant(String value) {
+		this.value = value;
+	}
+
+	public void print(int depth) {
+		printIndent(depth);
+		System.out.println("\"" + value + "\"");
 	}
 }
