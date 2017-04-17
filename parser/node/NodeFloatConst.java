ad2e40482610b2a2f8bb76e8cf528c3d848c78b8
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-17 23:48:27 +1000

    add class NodeFloatConst

---
+++ parser/node/NodeFloatConst.java
@@ -0,0 +1,23 @@
+package parser.node;
+
+import parser.Utils;
+
+/**
+ * Created by olegstotsky on 14.04.17.
+ */
+public class NodeFloatConst extends Node {
+    public double val;
+
+    public NodeFloatConst() {
+        this.val = 0;
+    }
+
+    public NodeFloatConst(double val) {
+        this.val = val;
+    }
+
+    public void print(int depth) {
+        Utils.printIndent(depth);
+        System.out.println(this.val);
+    }
+}
