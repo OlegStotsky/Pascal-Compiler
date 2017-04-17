848b1edb230aab709ae2feab6d4772dbc82f0288
Oleg Stotsky <medvedadskiy@gmail.com>
2017-04-17 23:45:19 +1000

    make SymTypeChar singletone

---
+++ parser/symbol/SymTypeChar.java
@@ -0,0 +1,23 @@
+package parser.symbol;
+
+import parser.Utils;
+
+/**
+ * Created by olegstotsky on 17.04.17.
+ */
+public class SymTypeChar extends SymType {
+    public static SymTypeChar instance = new SymTypeChar();
+
+    private SymTypeChar() {
+        super("char");
+    }
+
+    public static SymTypeChar getInstance() {
+        return instance;
+    }
+
+    public void print(int depth) {
+        Utils.printIndent(depth);
+        System.out.println("TYPE" + " " + this.name);
+    }
+}
