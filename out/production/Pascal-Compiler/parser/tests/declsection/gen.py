fda7b9a9345c0dcc9222d267d493a32289631399
Oleg Stotsky <medvedadskiy@gmail.com>
2017-07-24 13:38:55 +1000

    remove redundant directory

--- out/production/Pascal-Compiler/parser/tests/declsection/gen.py
+++
@@ -1,6 +0,0 @@
-from subprocess import check_output
-import sys
-
-*redundant, name, start, end = sys.argv
-for i in range(int(start), int(end)+1):
-	check_output("java -jar {0} {1}.in {1}.expected".format(name, i), shell=True)
--- out/production/Pascal-Compiler/parser/tests/declsection/rename.py
+++
@@ -1,5 +0,0 @@
-from subprocess import check_output
-
-for i in range(29, 36):
-	check_output('mv {0}.in {1}.in'.format(i, i-29), shell=True)
-	check_output('mv {0}.out {1}.out'.format(i, i-29), shell=True)
