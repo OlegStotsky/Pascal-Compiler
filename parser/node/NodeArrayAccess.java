80864949a5d88f34be78dedc27edebabda2286f9
Oleg Stotsky <medvedadskiy@gmail.com>
2017-07-25 15:39:28 +1000

    add method NodeArrayAccess.getType

--- parser/node/NodeArrayAccess.java
+++ parser/node/NodeArrayAccess.java
@@ -1,6 +1,9 @@
 package parser.node;
 
 import parser.Utils;
+import parser.symbol.SymTable;
+import parser.symbol.SymTypeArray;
+import parser.symbol.Symbol;
 
 public class NodeArrayAccess extends Node {
     public Node id;
@@ -14,6 +17,11 @@ public class NodeArrayAccess extends Node {
     public NodeArrayAccess(Node name, Node index) {
         this.id = name;
         this.index = index;
+    }
+
+    @Override
+    public Symbol getType(SymTable symTable) throws Exception {
+        return ((SymTypeArray)id.getType(symTable)).elemType;
     }
 
     public void print(int depth) throws Exception {
